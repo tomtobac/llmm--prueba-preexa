fdc8c8ee65c1225d9c20b82bf49b928bfbdba982
Tomeu Cabot <tomeu.cabot@trivago.com>
2018-01-23 12:23:10 +0100

    huehuehue

--- firstPage.js
+++ firstPage.js
@@ -1,14 +1,11 @@
-
-
-
-function ocultar(x){
-	var hide = document.getElementById(1);
-    hide.style.visibility ='hidden';	
-	hide.style.display='none';
-}
-
-function mostrar(x) {
-	var hide = document.getElementById(1);
-    hide.style.visibility ='visible';	
-	hide.style.display='';
+// https://developer.mozilla.org/es/docs/Web/API/GlobalEventHandlers/onload
+window.onload = function() {
+	var titles = document.querySelectorAll('.smallTitle');
+	titles.forEach(function(title) {
+		title.addEventListener('click', function(e) {
+			var target = e.target;
+			// https://developer.mozilla.org/en-US/docs/Web/API/NonDocumentTypeChildNode/nextElementSibling
+			target.nextElementSibling.classList.toggle('u-display-none');
+		});
+	});
 }
